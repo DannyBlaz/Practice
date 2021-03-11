b96bcbdaf40092f17784e3ce82ca755d2586434d
Daniel <dannyblaze469@gmail.com>
2021-03-11 05:21:38 -0500

    [My first practice Repo]

---
+++ ruby practice.rb
@@ -0,0 +1,43 @@
+# def coffee
+#   puts self
+# end
+
+# coffee
+
+
+# class Cat
+#   def meow
+#     puts self
+#   end
+# end
+# Cat.new.meow
+
+
+# class Example
+#   def do_something
+#     banana = "variable"
+#     puts banana
+#     puts self.banana
+#   end
+#   def banana
+#     "method"
+#   end
+# end
+# Example.new.do_something
+
+# class Salad
+#   def initialize
+#     @ingredients = []
+#   end
+#   def add_nuts
+#     @ingredients << :nuts
+#     p self
+#   end
+# end
+# my_salad = Salad.new.add_nuts
+
+# p [1,2,3,nil,4].select(&:itself)
+
+arr = [0,1,2,3,4,5]
+p arr.drop(2)
+p arr.take(2)
