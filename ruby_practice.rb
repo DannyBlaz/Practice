ef4cd29ef1ac9603f06b21f0eb8c0a092ed3ec73
Daniel <dannyblaze469@gmail.com>
2021-12-02 20:54:06 -0500

    Practice

--- ruby_practice.rb
+++ ruby_practice.rb
@@ -136,3 +136,15 @@ class PostMailer < ApplicationMailer
       subject: "New post created"
   end
 end
+
+function towerBuilder(nFloors) {
+  var space = "";
+  var star = "";
+  var result = [];
+  for (var i = 1; i <= nFloors; i++) {
+    space = (" ").repeat(nFloors - i);
+    star = ("*").repeat((2 * i) - 1);
+    result.push(space + star + space);
+  }
+  return result;
+}
