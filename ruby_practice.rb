48c6eabec1a300228422c5b5acbcfef8d62c12c4
Daniel <dannyblaze469@gmail.com>
2021-12-03 22:34:08 -0500

    Practice

--- ruby_practice.rb
+++ ruby_practice.rb
@@ -98,53 +98,72 @@ require 'byebug'
 #   end
 #   p new_arr
 # end;
-def explode arr
-  if arr.first.is_a?String
-    total = arr.last
-  elsif arr.last.is_a?String
-    total = arr.first
-  else
-    total = arr.sum
-  end
-  p total
-  new_arr = []
-  (1..total).each do |i|
-    new_arr << arr
-  end
-  p new_arr
-end
+# def explode arr
+#   if arr.first.is_a?String
+#     total = arr.last
+#   elsif arr.last.is_a?String
+#     total = arr.first
+#   else
+#     total = arr.sum
+#   end
+#   p total
+#   new_arr = []
+#   (1..total).each do |i|
+#     new_arr << arr
+#   end
+#   p new_arr
+# end
 
 # explode([9, "f"])
 # # p "46".is_a? String
-i = "Sudo Placements"
-for a in 1..5 do     
- puts a  
+# i = "Sudo Placements"
+# for a in 1..5 do     
+#  puts a  
+# end
+
+# function towerBuilder(nFloors) {
+#   var space = "";
+#   var star = "";
+#   var result = [];
+#   for (var i = 1; i <= nFloors; i++) {
+#     space = (" ").repeat(nFloors - i);
+#     star = ("*").repeat((2 * i) - 1);
+#     result.push(space + star + space);
+#   }
+#   return result;
+# }
+
+# def DNA_strand(dna)
+#   new_arr = []
+#   new_dna = dna.split("")
+#   new_dna.each do |char|
+#     if char == "A"
+#       new_arr << "T"
+#     elsif char == "T"
+#       new_arr << "A"
+#     elsif char == "G"
+#       new_arr << "C"
+#     elsif char == "C"
+#       new_arr << "G"
+#     end
+#   end
+#   new_arr.join("")
+# end
+
+# a = 'man'
+# p a.sub('a', '@')
+def DNA_strand(dna)
+  new_arr = []
+  dna.split("").each do |char|
+    new_arr << char.sub("A","T") if char == 'A'
+    new_arr << char.sub("T","A") if char == 'T'
+    new_arr << char.sub("C","G") if char == 'C'
+    new_arr << char.sub("G","C") if char == 'G'
+  end 
+  p new_arr.join
 end
 
-
-class PostMailer < ApplicationMailer
-  # Subject can be set in your I18n file at config/locales/en.yml
-  # with the following lookup:
-      en.post_mailer.post_created.subject
-  #
-  def post_created
-    @greeting = "Hi"
-    mail
-      to: User.first.email,
-      cc: User.all.pluck(:email),
-      bcc: "secret@corsego.com",
-      subject: "New post created"
-  end
-end
-
-function towerBuilder(nFloors) {
-  var space = "";
-  var star = "";
-  var result = [];
-  for (var i = 1; i <= nFloors; i++) {
-    space = (" ").repeat(nFloors - i);
-    star = ("*").repeat((2 * i) - 1);
-    result.push(space + star + space);
-  }
-  return result;
-}
+DNA_strand("AAAA")
+DNA_strand("ATTGC")
+DNA_strand("GTAC")
+DNA_strand("AAAA")
